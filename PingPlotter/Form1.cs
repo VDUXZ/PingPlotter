f32d14656862f059390b9d5ea512466414f0456a
Adam Grant <adam.co.grant@gmail.com>
2020-05-07 11:23:11 -0700

    Add project files.

---
+++ PingPlotter/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPlotter
+{
+    public partial class f1 : Form
+    {
+        Timer timer = new Timer();
+        int time = 0;
+
+        private void InitializeTimer()
+        {
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            Debug.WriteLine(time);
+            time++;
+        }
+
+        public f1()
+        {
+            InitializeTimer();
+        }
+    }
+}
---
+++ PingPlotter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace PingPlotter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form f = new f1
+            {
+                BackColor = Color.White,
+                FormBorderStyle = FormBorderStyle.FixedSingle
+            };
+
+            System.Drawing.Rectangle wSize = new Rectangle(0, 0, 1000, 400);
+
+            f.Bounds = wSize;
+            f.TopMost = true;
+            Application.EnableVisualStyles();
+            Application.Run(f);
+            Debug.WriteLine(Screen.PrimaryScreen.Bounds);
+            Console.WriteLine(Screen.PrimaryScreen.Bounds);
+        }
+    }
+}
